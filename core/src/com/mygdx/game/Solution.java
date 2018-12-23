ad5b8371ada03c438f5ec5e970c89b817bd0dd77
melih1996 <34304254+melih1996@users.noreply.github.com>
2018-12-23 12:25:24 +0300

    Add files via upload

---
+++ core/src/com/mygdx/game/Solution.java
@@ -0,0 +1,13 @@
+package newpackage;
+
+import java.util.Arrays;
+
+public class Solution {
+	public static void main(String[] args) {
+		while(true)
+			System.out.println("SEN BENÝM BAÞTACIMSIN KIYASLAMA KENDÝNÝ KÝMSEYLE SEN BENÝM GÜZEL KADINIMSIN DEÐÝÞMEM SENÝ HÝÇBÝR ÞEYLE");
+		
+	
+		
+	}
+}
