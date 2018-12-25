d1a65fc3d75a3f6cf6350171fb2f3b0bdb683e21
musabgelisgen <mgelisgen97@gmail.com>
2018-12-25 14:55:32 +0300

    card logic and assets added

--- core/src/com/mygdx/game/Card.java
+++ core/src/com/mygdx/game/Card.java
@@ -2,7 +2,7 @@ package com.mygdx.game;
 
 public class Card extends BaseActor {
 	int moveAmount;	//it shows how many units we can move totally
-	boolean shift;	//it shows whether we can shift the board
+	public boolean shift;	//it shows whether we can shift the board
 	boolean slice;	//it shows whether we can slice the car however we want
 	public Card(int moveAmount,boolean shift,boolean slice) {
 		this.moveAmount=moveAmount;
--- core/src/com/mygdx/game/HumanVsHuman.java
+++ core/src/com/mygdx/game/HumanVsHuman.java
@@ -36,6 +36,11 @@ public class HumanVsHuman extends BaseScreen {
 	Board[] boards;
 	TextButton drawCard;
 	int paddingW,paddingH;
+	int timer = 0;
+
+	BaseActor card, move, move1, move2, move3, shiftMap, skip;
+	Card currentCard;
+	boolean expectingMovement = false;
 	public HumanVsHuman(BaseGame game) {
 		super(game);
 		// TODO Auto-generated constructor stub
@@ -69,12 +74,12 @@ public class HumanVsHuman extends BaseScreen {
 		leftY=paddingH+mapHeight;
 		middleY=leftY;
 		rightY=middleY;
-		
+
 		boardX=leftX;
 		boardY=leftY-mapHeight;
 		boardWidth=mapWidth;
 		boardHeight=3*mapHeight;
-		
+
 		leftboard=new Board(0,6,5,6,leftX,leftY,1);
 		middleboard=new Board(5,6,4,6,middleX,middleY,2);
 		rightboard=new Board(9,6,5,6,rightX,rightY,3);
@@ -88,11 +93,12 @@ public class HumanVsHuman extends BaseScreen {
 				MainMenu.start=false;
 				game.skin.get("buton",Sound.class).play();
 				return true;
-		}
+			}
 			public void touchUp(InputEvent ev,float x,float y,int pointer,int button){
 				game.setScreen(new MultiPlayer(game));
 			}
-			});
+		});	
+
 		boards[0]=leftboard;
 		boards[1]=middleboard;
 		boards[2]=rightboard;
@@ -113,66 +119,101 @@ public class HumanVsHuman extends BaseScreen {
 				else if(i>2*m-1)
 					gameTable[i][j]=-1;
 			}
-		
-		
-		
+
+
+
 		for(final Car carx:cars){
-			
+
 			carx.addListener(new InputListener(){
-			
-				
-				 public boolean touchDown(InputEvent ev,float x,float y,int pointer,int button){
-							carx.firstTouchX=(int) (x/(mapWidth/n));
-							carx.firstTouchY=(int) (y/(mapHeight/m));
-							carx.lastX=carx.x+carx.firstTouchX;
-							carx.lastY=carx.y+carx.height-carx.firstTouchY;
-							if(carx.id!=3-turn)
-								carx.pressed=true;
-						
-						return true;
+
+
+				public boolean touchDown(InputEvent ev,float x,float y,int pointer,int button){
+					if (expectingMovement && !(currentCard.shift)) {
+						carx.firstTouchX=(int) (x/(mapWidth/n));
+						carx.firstTouchY=(int) (y/(mapHeight/m));
+						carx.lastX=carx.x+carx.firstTouchX;
+						carx.lastY=carx.y+carx.height-carx.firstTouchY;
+						if(carx.id!=3-turn)
+							carx.pressed=true;
 					}
-						public void touchUp(InputEvent ev,float x,float y,int pointer,int button){
-							carx.pressed=false;
-							
-						}
-			 });
-			}
+					return true;
+
+				}
+				public void touchUp(InputEvent ev,float x,float y,int pointer,int button){
+					carx.pressed=false;
+
+					if (expectingMovement && !(currentCard.shift)) {
+						expectingMovement = false;
+						moveCount = 0;
+					}
+
+				}
+			});
+		}
 		//board action listeners
 		for(final Board b:boards){
-			
+
 			b.addListener(new InputListener()
 			{
 				public boolean touchDown(InputEvent ev,float x,float y,int pointer,int button){
-					
-//					for(int i=0;i<gameTable.length;i++)
-//						System.out.println(Arrays.toString(gameTable[i]));
-//					System.out.println("\n\n");
-					boolean shift=true;
-//					for(int i=0;i<gameTable.length;i++)
-//						if((gameTable[i][4]>0 && gameTable[i][5]==gameTable[i][4] && (b.id==1 || b.id==2))||(gameTable[i][8]>0 && gameTable[i][8]==gameTable[i][9] && (b.id==3 || b.id==2)))
-//						{
-//							System.out.println("id:"+b.id+" sýkýntý:"+i);
-//							shift=false;
-//							break;
-//						}
-					b.firstTouchX=(int) (x/(mapWidth/n));
-					b.firstTouchY=b.height-(int) (y/(mapHeight/m))-1;
-					//b.lastX=b.firstTouchX+b.x;
-					//b.lastY=b.firstTouchY+b.y;
-					b.pressed=shift;
-					
-					
+					if (expectingMovement && currentCard.shift) {
+						//					for(int i=0;i<gameTable.length;i++)
+						//						System.out.println(Arrays.toString(gameTable[i]));
+						//					System.out.println("\n\n");
+						boolean shift=true;
+						//					for(int i=0;i<gameTable.length;i++)
+						//						if((gameTable[i][4]>0 && gameTable[i][5]==gameTable[i][4] && (b.id==1 || b.id==2))||(gameTable[i][8]>0 && gameTable[i][8]==gameTable[i][9] && (b.id==3 || b.id==2)))
+						//						{
+						//							System.out.println("id:"+b.id+" sï¿½kï¿½ntï¿½:"+i);
+						//							shift=false;
+						//							break;
+						//						}
+						b.firstTouchX=(int) (x/(mapWidth/n));
+						b.firstTouchY=b.height-(int) (y/(mapHeight/m))-1;
+						//b.lastX=b.firstTouchX+b.x;
+						//b.lastY=b.firstTouchY+b.y;
+						b.pressed=shift;
+
+					}
 					return true;
-			}
+				}
 				public void touchUp(InputEvent ev,float x,float y,int pointer,int button){
 					b.pressed=false;
+					expectingMovement = false;
+					moveCount = 0;
 				}
-				});
+			});
 		}
+
+		card = new BaseActor();
+		card.setTexture(new Texture("card2.png"));
+		card.setVisible(false);
 		
+		shiftMap = new BaseActor();
+		shiftMap.setTexture(new Texture("shift.png"));
+		shiftMap.setVisible(false);
 		
-			
+		move = new BaseActor();
+		move.setTexture(new Texture("move.png"));
+		move.setVisible(false);
 		
+		move1 = new BaseActor();
+		move1.setTexture(new Texture("1.png"));
+		move1.setVisible(false);
+		
+		move2 = new BaseActor();
+		move2.setTexture(new Texture("2.png"));
+		move2.setVisible(false);
+		
+		move3 = new BaseActor();
+		move3.setTexture(new Texture("3.png"));
+		move3.setVisible(false);
+		
+		skip = new BaseActor();
+		skip.setTexture(new Texture("skip.png"));
+		skip.setVisible(false);
+
+
 		uiTable.setBackground(game.skin.getDrawable("background"));
 		uiTable.add(returnMenu);
 		leftboard.setVisible(true);
@@ -182,12 +223,102 @@ public class HumanVsHuman extends BaseScreen {
 		uiStage.addActor(rightboard);
 		uiStage.addActor(middleboard);
 		uiStage.addActor(drawCard);
+		uiStage.addActor(card);
+		uiStage.addActor(shiftMap);
+		uiStage.addActor(move);
+		uiStage.addActor(move1);
+		uiStage.addActor(move2);
+		uiStage.addActor(move3);
+		uiStage.addActor(skip);
+
+
+		drawCard.addListener(new InputListener()
+		{
+			public boolean touchDown(InputEvent ev,float x,float y,int pointer,int button){
+
+				return true;
+			}
+			public void touchUp(InputEvent ev,float x,float y,int pointer,int button){
+				if (!expectingMovement) {
+					int moveCount = (int)(Math.random() * 3) + 1;
+					boolean shift = false;
+					boolean slice = false;
+
+					if ((int)(Math.random() * 4) == 0) {
+						shift = true;
+					}
+					else {
+						shift = false;
+					}
+					
+					if ((int)(Math.random() * 4) != 0) {
+						slice = false;
+						targetMove = moveCount;
+					}
+					else {
+						slice = true;
+						targetMove = 200;
+					}
+
+					currentCard = new Card(moveCount, shift, slice);
+					expectingMovement = true;
+					System.out.println("Card: " + moveCount + " " + shift + " " + slice);
+					
+					timer = 100;
+					
+					card.setVisible(true);
+					card.setPosition(VIEW_WIDTH/2-95, VIEW_HEIGHT/2-133);
+					card.setWidth(190);
+					card.setHeight(266);
+					
+					if (shift) {
+						shiftMap.setVisible(true);
+						shiftMap.setPosition(VIEW_WIDTH/2-95, VIEW_HEIGHT/2-133);
+						shiftMap.setWidth(190);
+						shiftMap.setHeight(266);
+					}
+					else if (slice) {
+						skip.setVisible(true);
+						skip.setPosition(VIEW_WIDTH/2-50, VIEW_HEIGHT/2-50);
+						skip.setWidth(100);
+						skip.setHeight(100);
+					}
+					else {
+						move.setVisible(true);
+						move.setPosition(VIEW_WIDTH/2-50, VIEW_HEIGHT/2-100);
+						move.setWidth(100);
+						move.setHeight(100);
+						
+						if (moveCount == 1) {
+							move1.setVisible(true);
+							move1.setPosition(VIEW_WIDTH/2-50, VIEW_HEIGHT/2 + 10);
+							move1.setWidth(100);
+							move1.setHeight(100);
+						}
+						else if (moveCount == 2) {
+							move2.setVisible(true);
+							move2.setPosition(VIEW_WIDTH/2-50, VIEW_HEIGHT/2 + 10);
+							move2.setWidth(100);
+							move2.setHeight(100);
+						}
+						else {
+							move3.setVisible(true);
+							move3.setPosition(VIEW_WIDTH/2-50, VIEW_HEIGHT/2 + 10);
+							move3.setWidth(100);
+							move3.setHeight(100);
+						}
+					}
+				}
+			}
+		});
+
+
 		for(Board b:boards)
 			b.setPosition(b.leftX,b.leftY);
 		//uiTable.add(leftboard);
 		//uiTable.add(rightboard);
 		//uiTable.add(middleboard);
-		
+
 
 	}
 	public void createCars(ArrayList<Car> list){
@@ -199,33 +330,33 @@ public class HumanVsHuman extends BaseScreen {
 		Car car6=new Car(1,5,7,1,2,0);
 		Car car7=new Car(0,2,6,2,1,0);
 		Car car8=new Car(0,6,7,2,1,2);	//Player2
-		
-		
-		
+
+
+
 		car1.setTexture(new Texture("ferrari.png"));
 		car1.setPosition(boardX+car1.x*mapWidth/n, boardY+boardHeight*car1.y/gameTable.length);
-		
+
 		car2.setTexture(new Texture("bugatti.png"));
 		car2.setPosition(boardX+car2.x*mapWidth/n, boardY+boardHeight*car2.y/gameTable.length);
-		
+
 		car3.setTexture(new Texture("ambulance.png"));
 		car3.setPosition(boardX+car3.x*mapWidth/n, boardY+boardHeight*car3.y/gameTable.length);
-				
+
 		car4.setTexture(new Texture("chevrolet.png"));
 		car4.setPosition(boardX+car4.x*mapWidth/n, boardY+boardHeight*car4.y/gameTable.length);
-						
+
 		car5.setTexture(new Texture("jeep.png"));
 		car5.setPosition(boardX+car5.x*mapWidth/n, boardY+boardHeight*car5.y/gameTable.length);
-						
+
 		car6.setTexture(new Texture("dashcam.png"));
 		car6.setPosition(boardX+car6.x*mapWidth/n, boardY+boardHeight*car6.y/gameTable.length);
-						
+
 		car7.setTexture(new Texture("mercedes.png"));
 		car7.setPosition(boardX+car7.x*mapWidth/n, boardY+boardHeight*car7.y/gameTable.length);
-		
+
 		car8.setTexture(new Texture("mercedes.png"));
 		car8.setPosition(boardX+car8.x*mapWidth/n, boardY+boardHeight*car8.y/gameTable.length);
-				
+
 		list.add(car1);
 		list.add(car2);
 		list.add(car3);
@@ -234,7 +365,7 @@ public class HumanVsHuman extends BaseScreen {
 		list.add(car6);
 		list.add(car7);
 		list.add(car8);
-		
+
 		uiStage.addActor(car1);
 		uiStage.addActor(car2);
 		uiStage.addActor(car3);
@@ -247,7 +378,7 @@ public class HumanVsHuman extends BaseScreen {
 	}
 	@Override
 	public void render(float dt) {
-		
+
 		super.render(dt);
 		//Positions
 		for(Board b:boards)
@@ -259,24 +390,37 @@ public class HumanVsHuman extends BaseScreen {
 		rightboard.setWidth(rightwidth);
 		leftboard.setWidth(leftwidth);
 		middleboard.setWidth(middlewidth);
-		
+
 		//Heights
 		rightboard.setHeight(mapHeight);
 		leftboard.setHeight(mapHeight);
 		middleboard.setHeight(mapHeight);
+
+		//		ShapeRenderer sr=new ShapeRenderer();
+		//		sr.begin(ShapeType.Line);
+		//		sr.setColor(Color.YELLOW);
+		//		for(int i=0;i<m;i++)
+		//			sr.line(leftX, leftY+mapHeight*i/m, rightX+rightwidth, leftY+mapHeight*i/m);
+		//		for(int i=0;i<n;i++)
+		//			sr.line(leftX+mapWidth*i/n, leftY, leftX+mapWidth*i/n, leftY+mapHeight);
+		//		sr.end();
+		//		for(Car car:cars)
+		//			car.setPosition(boardX+car.x*mapWidth/n, boardY+boardHeight*car.y/gameTable.length);
+		//	
 		
-//		ShapeRenderer sr=new ShapeRenderer();
-//		sr.begin(ShapeType.Line);
-//		sr.setColor(Color.YELLOW);
-//		for(int i=0;i<m;i++)
-//			sr.line(leftX, leftY+mapHeight*i/m, rightX+rightwidth, leftY+mapHeight*i/m);
-//		for(int i=0;i<n;i++)
-//			sr.line(leftX+mapWidth*i/n, leftY, leftX+mapWidth*i/n, leftY+mapHeight);
-//		sr.end();
-//		for(Car car:cars)
-//			car.setPosition(boardX+car.x*mapWidth/n, boardY+boardHeight*car.y/gameTable.length);
-//		
-		
+		if (timer > 0) {
+			timer = timer - 1;
+			if (timer == 0) {
+				card.setVisible(false);
+				shiftMap.setVisible(false);
+				move.setVisible(false);
+				skip.setVisible(false);
+				move1.setVisible(false);
+				move2.setVisible(false);
+				move3.setVisible(false);
+			}
+		}
+
 	}
 
 	@Override
@@ -292,23 +436,23 @@ public class HumanVsHuman extends BaseScreen {
 		}
 		//Positions
 		returnMenu.setPosition(0, 0);
-		
-		
+
+
 	}
-	
+
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
 		// upperleft is (0,0)	
-//		for(Car c:cars){
-//			int s=0;ArrayList<Board> cross=new ArrayList<Board>();
-//			for(Board b:boards)
-//				if(b.x<=c.x)
-//					if(b.x+b.width>=c.x+c.width)
-//						cross.add(b);
-//			if(cross.size()>1)
-//				for(Board b:cross)
-//					b.pressed=false;
-//		}
+		//		for(Car c:cars){
+		//			int s=0;ArrayList<Board> cross=new ArrayList<Board>();
+		//			for(Board b:boards)
+		//				if(b.x<=c.x)
+		//					if(b.x+b.width>=c.x+c.width)
+		//						cross.add(b);
+		//			if(cross.size()>1)
+		//				for(Board b:cross)
+		//					b.pressed=false;
+		//		}
 		int a=boardX;
 		int b=boardY;
 		//(a,b) is the lowerleft corner
@@ -319,12 +463,12 @@ public class HumanVsHuman extends BaseScreen {
 		a1/=w1;
 		int b1=sc-b;
 		b1/=h1;
-//		for(int i=0;i<gameTable.length;i++)
-//			System.out.println(Arrays.toString(gameTable[i]));
-//		System.out.println("\n\n");
-//		System.out.println(gameTable[6][8]+" , "+gameTable[6][9]);
+		//		for(int i=0;i<gameTable.length;i++)
+		//			System.out.println(Arrays.toString(gameTable[i]));
+		//		System.out.println("\n\n");
+		//		System.out.println(gameTable[6][8]+" , "+gameTable[6][9]);
 		for(int i=0;i<boards.length;i++){
-			
+
 			int bb1=screenY-(VIEW_HEIGHT-boardY-boardHeight);
 			bb1/=h1;
 			if(boards[i].pressed)
@@ -337,37 +481,37 @@ public class HumanVsHuman extends BaseScreen {
 				if(boards[i].pressed)
 					if((bb1-boards[i].firstTouchY+boards[i].height<boards[i-1].y)||(bb1-boards[i].firstTouchY>boards[i-1].y+boards[i-1].height))
 						boards[i].pressed=false;
-					
-			
+
+
 			if(boards[i].pressed){
-				
+
 				boards[i].dragBoard(boards[i].x,bb1-boards[i].firstTouchY, gameTable, boardX, boardY, boardWidth,boardHeight,cars);
-				
+
 			}
 		}
 		for(int i=0;i<cars.size();i++){
-			
-				
+
+
 			if(cars.get(i).pressed && cars.get(i).move){
-					if(cars.get(i).setPosition(a1-cars.get(i).firstTouchX,b1-cars.get(i).firstTouchY,gameTable,i,w1,h1,a,b,cars))
-						moveCount++;
-					if(moveCount==targetMove){
-						cars.get(i).pressed=false;
-						turn=3-turn;
-//						for(int i=0;i<2;i++)
-//							players_played[i]=!players_played[i];
-						
-						moveCount=0;
-					}
-					
+				if(cars.get(i).setPosition(a1-cars.get(i).firstTouchX,b1-cars.get(i).firstTouchY,gameTable,i,w1,h1,a,b,cars))
+					moveCount++;
+				if(moveCount==targetMove){
+					cars.get(i).pressed=false;
+					turn=3-turn;
+					//						for(int i=0;i<2;i++)
+					//							players_played[i]=!players_played[i];
+
+					moveCount=0;
+				}
+
 			}
-		
-		
-	}
-		
+
+
+		}
+
 		return true;
 	}
-	
-	
+
+
 
 }
