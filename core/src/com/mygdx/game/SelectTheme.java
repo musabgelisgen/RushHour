e7796664aef6cece99777d3f785b35b4da15a003
avci <ahmetabdullahavci07@gmail.com>
2018-12-25 09:31:19 +0300

    pull problem

--- core/src/com/mygdx/game/SelectTheme.java
+++ core/src/com/mygdx/game/SelectTheme.java
@@ -9,9 +9,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class SelectTheme extends BaseScreen {
 	TextButton returnMenu;
-	TextButton newTheme1,newTheme2,newTheme3,newTheme4;
+	TextButton newTheme1,newTheme2;
 	TextButton[] themeButtons;
 	static Texture targetTexture = new Texture("grid.png");
+	BaseActor frame1,frame2;
 	static int targetNumber = 1;
 	public SelectTheme(BaseGame game) {
 		super(game);
@@ -19,16 +20,21 @@ public class SelectTheme extends BaseScreen {
 
 	@Override
 	public void create() {
+		frame1 = new BaseActor();
+		frame1.setTexture(new Texture("frame.jpg"));
+		
+		frame2 = new BaseActor();
+		frame2.setTexture(new Texture("frame.jpg"));
+		
+		
 
-		themeButtons = new TextButton[4];
+		themeButtons = new TextButton[2];
 		newTheme1 = new TextButton(targetNumber == 1 ? "CHOSEN": "", game.skin, "buttonTheme1");
 		newTheme2 = new TextButton(targetNumber == 2 ? "CHOSEN": "", game.skin, "buttonTheme2");
-		newTheme3 = new TextButton(targetNumber == 3 ? "CHOSEN": "", game.skin, "buttonTheme3");
-		newTheme4 = new TextButton(targetNumber == 4 ? "CHOSEN": "", game.skin, "buttonTheme4");
+//		newTheme3 = new TextButton(targetNumber == 3 ? "CHOSEN": "", game.skin, "buttonTheme3");
+//		newTheme4 = new TextButton(targetNumber == 4 ? "CHOSEN": "", game.skin, "buttonTheme4");
 		themeButtons[0] = newTheme1;
 		themeButtons[1] = newTheme2;
-		themeButtons[2] = newTheme3;
-		themeButtons[3] = newTheme4;
 		
 		returnMenu=new TextButton("Back", game.skin, "buttonStyle3");
 		returnMenu.addListener(new InputListener()
@@ -75,47 +81,49 @@ public class SelectTheme extends BaseScreen {
 			}
 		});
 		
-		newTheme3.addListener(new InputListener()
-		{
-			public boolean touchDown(InputEvent ev, float x, float y, int pointer, int button) {
-				game.skin.get("buton", Sound.class).play();
-				// Clean the texts
-				for (int i = 0; i < themeButtons.length; i++) {
-					themeButtons[i].setText("");
-				}
-				return true;
-			}
-			public void touchUp(InputEvent ev, float x, float y, int pointer, int button) {
-				newTheme3.setText("CHOSEN");
-				targetTexture = game.skin.get("themeTexture3", Texture.class);
-				targetNumber = 3;
-			}
-		});
-		
-		newTheme4.addListener(new InputListener()
-		{
-			public boolean touchDown(InputEvent ev, float x, float y, int pointer, int button) {
-				game.skin.get("buton", Sound.class).play();
-				// Clean the texts
-				for (int i = 0; i < themeButtons.length; i++) {
-					themeButtons[i].setText("");
-				}
-				return true;
-			}
-			public void touchUp(InputEvent ev, float x, float y, int pointer, int button) {
-				newTheme4.setText("CHOSEN");
-				targetTexture = game.skin.get("themeTexture4", Texture.class);
-				targetNumber = 4;
-			}
-		});
+//		newTheme3.addListener(new InputListener()
+//		{
+//			public boolean touchDown(InputEvent ev, float x, float y, int pointer, int button) {
+//				game.skin.get("buton", Sound.class).play();
+//				// Clean the texts
+//				for (int i = 0; i < themeButtons.length; i++) {
+//					themeButtons[i].setText("");
+//				}
+//				return true;
+//			}
+//			public void touchUp(InputEvent ev, float x, float y, int pointer, int button) {
+//				newTheme3.setText("CHOSEN");
+//				targetTexture = game.skin.get("themeTexture3", Texture.class);
+//				targetNumber = 3;
+//			}
+//		});
+//		
+//		newTheme4.addListener(new InputListener()
+//		{
+//			public boolean touchDown(InputEvent ev, float x, float y, int pointer, int button) {
+//				game.skin.get("buton", Sound.class).play();
+//				// Clean the texts
+//				for (int i = 0; i < themeButtons.length; i++) {
+//					themeButtons[i].setText("");
+//				}
+//				return true;
+//			}
+//			public void touchUp(InputEvent ev, float x, float y, int pointer, int button) {
+//				newTheme4.setText("CHOSEN");
+//				targetTexture = game.skin.get("themeTexture4", Texture.class);
+//				targetNumber = 4;
+//			}
+//		});
 
 
 		uiTable.setBackground(game.skin.getDrawable("background_blurred"));
-
+		
+		uiStage.addActor(frame1);
+		uiStage.addActor(frame2);
 		uiStage.addActor(newTheme1);
 		uiStage.addActor(newTheme2);
-		uiStage.addActor(newTheme3);
-		uiStage.addActor(newTheme4);
+//		uiStage.addActor(newTheme3);
+//		uiStage.addActor(newTheme4);
 
 		uiTable.add(returnMenu);
 
@@ -124,13 +132,25 @@ public class SelectTheme extends BaseScreen {
 	@Override
 	public void update(float dt) {
 		// TODO Auto-generated method stub
+	
+		frame1.setWidth(250);
+		frame1.setHeight(180);
+		frame1.setPosition(150,250);
+		
+		frame2.setWidth(250);
+		frame2.setHeight(180);
+		frame2.setPosition(200,100);
+		
+		
+		int cbx = 70;
+		themeButtons[0].setWidth(3 * cbx);
+		themeButtons[0].setHeight(2*cbx);
+		themeButtons[0].setPosition(200,300);
+		
+		themeButtons[1].setWidth(3 * cbx);
+		themeButtons[1].setHeight(2*cbx);
+		themeButtons[1].setPosition(200,100);
 
-		for(int i=0;i<themeButtons.length;i++){
-			int cbx = 70;
-			themeButtons[i].setWidth(2 * cbx);
-			themeButtons[i].setHeight(cbx);
-			themeButtons[i].setPosition((cbx)*3*(i%(themeButtons.length/2))+VIEW_WIDTH/4 - 20, 3*VIEW_HEIGHT/4-(cbx+10)*2*(i/(themeButtons.length/2)));
-		}
 
 		returnMenu.setPosition(30, 0);
 		returnMenu.setWidth(180);
